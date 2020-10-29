99114701b91667dc53ec44c69f3954998a5abe4e
snk12 <swapnil.kharade1210@gmail.com>
2020-10-29 19:58:58 +0530

    header and footer

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -1,12 +1,31 @@
 import React from 'react';
 import { Card, CardImg, CardText, CardBody,CardTitle } from 'reactstrap';
 
-
+function RenderDish ( {dish}) {
+    return(
+       
+                <div  className="col-12 col-md-5 m-1">
+                    <Card>
+                        <CardImg top src={dish.image} alt={dish.name} />
+                        <CardBody>
+                            <CardTitle>{dish.name}</CardTitle>
+                            <CardText>{dish.description}</CardText>
+                        </CardBody>
+                    </Card>
+                </div>
+                
+            
+    );
+}
     
-    function RenderComments ( { dish , comments }) {
+    function RenderComments ( {comments }) {
         
-          const lists= 
-         comments.map((x) => {
+        
+          return ( 
+            <div className="col-12 col-md-5 m-1">
+                            <h4>Comments</h4>
+                            <ul className="list-unstyled">
+                                {comments.map((x) => {
               return (
                   <div>
                      <ul className="list-unstyled">
@@ -17,49 +36,33 @@ import { Card, CardImg, CardText, CardBody,CardTitle } from 'reactstrap';
                      </ul>
                   </div>
               );
-          }) 
-          return ( 
-            <div key={dish.id} className="col-12 col-md-5 m-1">
-                            <h4>Comments</h4>
-                            <div> { lists } </div>
+          }) }
+                            </ul>
             </div>
   
           );
         }
-        function RenderDish ( {dish}) {
-            return(
-               
-                        <div  className="col-12 col-md-5 m-1">
-                            <Card>
-                                <CardImg top src={dish.image} alt={dish.name} />
-                                <CardBody>
-                                    <CardTitle>{dish.name}</CardTitle>
-                                    <CardText>{dish.description}</CardText>
-                                </CardBody>
-                            </Card>
-                        </div>
-                        
-                    
-            );
-        }
+        
             
     
     const DishDetail = (props) => {
+        if(props.dish != null){
         return(
             <div className="container">
                 <div className="row">
                      <RenderDish dish={ props.dish } />
-                     <RenderComments dish={ props.dish } comments={ props.dish.comments } />
+                     <RenderComments comments={ props.dish.comments } />
                  </div>
             </div>
                  
                
         );
+        }
+     else{
+         return(<div></div>);
+     }
     } 
        
 
 
 export default DishDetail;
-
-
-
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './MenuComponent';
 import DishDetail from './DishDetailComponent';
+import Header from './HeaderComponent';
+import Footer from './FooterComponent';
 import { DISHES } from '../shared/dishes';
 
 class Main extends Component {
@@ -14,22 +15,18 @@ class Main extends Component {
 }
 
 onDishSelect(dishId) {
-    this.setState({ selectedDish: dishId});
+  this.setState({ selectedDish: dishId});
 }
+
 
 render() {
   return (
     
      <div>
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-          </div>
-        </Navbar>
-        <Menu dishes={this.state.dishes}
-            onClick={(dishId) => this.onDishSelect(dishId)}
-        />
-        <DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
+         <Header />
+         <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)} />
+        <DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />   
+        <Footer />
       
       </div>
     
