6a6c7b2706535bd4a7f97a4976ecddb6f1bb8853
snk12 <swapnil.kharade1210@gmail.com>
2020-10-30 07:54:16 +0530

    React Router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Navbar, NavbarBrand } from 'reactstrap';
+import { BrowserRouter } from 'react-router-dom';
 import Main from './components/MainComponent';
 
 
@@ -9,11 +9,11 @@ class App extends Component {
 render() {
   return (
     
-     <div>
-        
-       <Main />
-      
-      </div>
+    <BrowserRouter>
+      <div className="App">
+          <Main />
+       </div>
+    </BrowserRouter>
     
   );
 }
