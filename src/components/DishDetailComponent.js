cc6428058459bb2a0287a60adc96ed7363bea342
snk12 <swapnil.kharade1210@gmail.com>
2020-11-01 21:17:22 +0530

    Fetch from Server

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -4,6 +4,7 @@ import { Card, CardImg, CardText, CardBody,
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Link } from 'react-router-dom';
 import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
 
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
 const minLength = (len) => (val) => val && (val.length >= len);
@@ -13,7 +14,7 @@ function RenderDish ( {dish}) {
        
                 <div >
                     <Card>
-                        <CardImg top src={dish.image} alt={dish.name} />
+                        <CardImg top src={baseUrl + dish.image} alt={dish.name} />  
                         <CardBody>
                             <CardTitle>{dish.name}</CardTitle>
                             <CardText>{dish.description}</CardText>
